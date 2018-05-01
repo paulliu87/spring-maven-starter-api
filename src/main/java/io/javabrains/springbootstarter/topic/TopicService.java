3a9c2b592481f2a4ee3b8a1fe889ea165bae83a8
paulliu87 <paul.pliu87@gmail.com>
2018-04-30 23:49:59 -0700

    create a new topic

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,18 +1,19 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Freamework", "Spring Framework Description"),
 			new Topic("java", "Java Freamework", "Java Framework Description"),
 			new Topic("ruby", "Ruby Freamework", "Ruby Framework Description")
-		    );
+		    ));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -20,5 +21,9 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
